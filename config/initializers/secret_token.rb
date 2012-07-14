c8e429be6232ffdbf623165aea2f1cd7fa7086d2
busyboard <contact@bb.c-coool.com>
2012-07-14 16:34:28 +0200

    Initial Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Character::Application.config.secret_token = '1cd5754a6dc53a8777dd936c795a1760bbcaef300d34cca6e31b150f3cadf3bd331c02da3fda2f836a24bc1481208fb07c58521ef3f6688530297b6d04aa80b9'
